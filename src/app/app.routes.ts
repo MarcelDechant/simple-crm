7fa0bfa3c47019fe2d263f11242b3aa0e6ed7073
Marcel Dechant <marcel.dechant@freenet.de>
2024-07-11 15:53:35 +0200

    text fields

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: DashbordComponent },
+    { path: 'dashbord', component: DashbordComponent },
+    { path: 'user', component: UserComponent },
+];
---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss'
+})
+export class DialogAddUserComponent {
+
+}
